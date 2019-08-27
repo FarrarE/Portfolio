8f345f9ee340a42c619c1dbf8e66bfe1724b8377
urza <efarrar@pdx.edu>
2019-08-27 12:44:37 -0700

    Updated css so that it is cleaner and added a nav bar that uses js.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function changeTab(evt, tab) {
-    var i, tabcontent, tablinks;
+    let i, tabcontent, tablinks;
 
     tabcontent = document.getElementsByClassName("tabcontent");
 
@@ -16,3 +16,32 @@ function changeTab(evt, tab) {
     document.getElementById(tab).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+
+
+function moveToSlide1() {
+    var slide1 = document.getElementById("slide1");
+    var slide1Offset = slide1.offsetTop;
+    var slide1Position = slide1Offset;
+    window.scrollTo(0, slide1Position);
+}
+
+function moveToSlide(e, slide) {
+    var slide = document.getElementById(slide);
+    var slideOffset = slide.offsetTop;
+    var slidePosition = slideOffset;
+    window.scrollTo(0, slidePosition);
+}
+
+// Event Listeners
+
+window.addEventListener('scroll', function(e) {
+      let nav = document.getElementById("nav");
+      let y = window.scrollY;
+
+      if (y >= 100) {
+          nav.className = "nav show"
+      } else {
+          nav.className = "nav hide"
+      }
+});
