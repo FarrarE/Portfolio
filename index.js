146bf92d234c881015be92f45a1d0d4ac8be70af
urza <efarrar@pdx.edu>
2019-08-27 11:03:23 -0700

    Changed js function name to 'changeTab'.

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-function openCity(evt, tab) {
+function changeTab(evt, tab) {
     var i, tabcontent, tablinks;
+
     tabcontent = document.getElementsByClassName("tabcontent");
+
     for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
+
     tablinks = document.getElementsByClassName("tablinks");
+
     for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+    
     document.getElementById(tab).style.display = "block";
     evt.currentTarget.className += " active";
 }
